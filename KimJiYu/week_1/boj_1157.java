0a98894d96b1131678b9959e90f35c2e11a05077
ttrlf <qmdlxlr@gmail.com>
2024-10-11 23:54:48 +0900

    [week2] KimJiYu

--- KimJiYu/week_1/boj_1157.java
+++ KimJiYu/week_1/boj_1157.java
@@ -1,2 +1,34 @@
-package nestnet_algorithm_2024_2_beginner.KimJiYu.week_1;public class boj_1157 {
+package nestnet_algorithm_2024_2_beginner.KimJiYu.week_1;
+
+import java.util.Scanner;
+
+public class boj_1157 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = new int[26];
+        String str = scanner.next();
+
+        for (int i = 0; i < str.length(); i++) {
+            if ('A' <= str.charAt(i) && str.charAt(i) <= 'Z') {
+                arr[str.charAt(i) - 'A']++;
+            } else {
+                arr[str.charAt(i) - 'a']++;
+            }
+        }
+
+        int max = -1;
+        char ch = '?';
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                ch = (char) (i + 65);
+            } else if (arr[i] == max) {
+                ch = '?';
+            }
+        }
+
+        System.out.print(ch);
+    }
 }
