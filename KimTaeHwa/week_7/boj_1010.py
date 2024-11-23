f3b62bdb3a6cca272dddef72d30a69eac9b91949
TaeHwa Kim <nomppi2@chungbuk.ac.kr>
2024-11-23 17:07:05 +0900

    (#78) [week7] KimTaeHwa

--- KimTaeHwa/week_7/boj_1010.py
+++ KimTaeHwa/week_7/boj_1010.py
@@ -1,2 +1,13 @@
 # 다리놓기
 
+def factorial(n): #n! 계산
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+a = int(input())
+
+for i in range(a):
+    n, m = map(int, input().split())
+    print(factorial(m)//(factorial(m-n)*factorial(n)))
